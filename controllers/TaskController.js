e12e3e71ac7f8848e3180c4c957a5edfce96c4c3
GuileSoler87 <guillermosoler87@gmail.com>
2023-04-27 11:02:36 +0200

    endpoint getbyid added

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -3,27 +3,42 @@ const Task = require("../models/Task");
 const TaskController = {
 
     async create(req, res) {
-        try{
+        try {
 
             const task = await Task.create(req.body)
 
-            res.status(201).send({message: "Task created", task})
+            res.status(201).send({ message: "Task created", task })
 
-        } catch(error) {
+        } catch (error) {
             console.error(error)
-            res.status(500).send({message: "Error creating task", error})
+            res.status(500).send({ message: "Error creating task", error })
         }
     },
 
     async getAll(req, res) {
         try {
             const tasks = await Task.find()
+
             res.status(200).send(tasks)
 
         } catch (error) {
             console.error(error)
-            res.status(500).send({message: "Error getting task", error})
+            res.status(500).send({ message: "Error getting tasks", error })
 
+        }
+    },
+
+    async getById(req, res) {
+        try {
+            const task = await Task.findById(req.params._id)
+            if (!task) {
+                return res.status(404).send({ message: 'Task not found' })
+            };
+            res.status(200).send(task)
+
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: "Error getting task", error })
         }
     }
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,5 +4,6 @@ const TaskController = require("../controllers/TaskController")
 
 router.post("/create", TaskController.create)
 router.get("/getAll", TaskController.getAll)
+router.get("/getById/:_id", TaskController.getById)
 
 module.exports = router;
