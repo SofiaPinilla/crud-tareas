f4f616c667d7d03c9c13fc08f92fb715a91da9aa
SofiaPinilla <contact@sofiapinilla.com>
2023-04-28 10:32:35 +0200

    documentation added

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const { dbConnection } = require('./config/config');
-
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
 
 app.use(express.json());
 
 app.use("/tasks", require("./routes/tasks"))
+
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))//ruta para ver la documentación de mi API
 
 dbConnection();
 
